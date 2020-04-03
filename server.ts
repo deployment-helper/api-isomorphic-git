3177f6ad1760f49da7bf10d1431b272806b18ce7
Vinay Mavi <vinaymavi@gmail.com>
2020-04-03 18:24:43 +0530

    Typescipt configuration

--- server.ts
+++ server.ts
@@ -4,8 +4,8 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req:any, res:any) => {
     res.send("Hello World");
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
