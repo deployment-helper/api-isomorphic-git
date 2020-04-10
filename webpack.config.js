f2d8de2bf5e2e2f7b83370c58b7e75a05a298288
Vinay Mavi <vinaymavi@gmail.com>
2020-04-10 09:58:37 +0530

    webpack config added for appengine deployment.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: './server.ts',
   target: "node",
@@ -19,4 +19,10 @@ module.exports = {
     filename: 'server.js',
     path: path.resolve(__dirname, 'dist'),
   },
+   plugins: [
+    new CopyPlugin([
+      { from: './app.yaml', to: './app.yaml' },
+      { from: './package.json', to: './package.json' },
+    ]),
+  ],
 };
