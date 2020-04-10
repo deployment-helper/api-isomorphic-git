5eb4bc656a14499ee042c610648d197b213dd3b6
Vinay Mavi <vinaymavi@gmail.com>
2020-04-10 14:06:36 +0530

    Application Development message added.

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get("/", (req:any, res:any) => {
-    res.send("Hello World");
+    res.send("Application Under Development");
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
