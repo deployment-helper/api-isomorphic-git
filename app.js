6329257428867c1e57b8da93a1a6474b0f93e901
Vinay Mavi <vinaymavi@gmail.com>
2020-04-11 01:10:39 +0530

    app.yaml auto generation logic added.

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log(`# Auto generated file
+runtime: ${process.env.runtime || 'nodejs12'}
+instance_class: ${process.env.instance_class || 'F1'}
+automatic_scaling: 
+    min_instances: ${process.env.min_instances || 1}
+    max_instances: ${process.env.max_instances || 1}
+    max_concurrent_requests: ${process.env.max_concurrent_requests || 50}
+env_variables:
+    DATABASE_NAME: ${process.env.DATABASE_NAME}
+`);
