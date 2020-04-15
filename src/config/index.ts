7a67f4934752c122b01cf2407a37105f4a38e955
Vinay Mavi <vinaymavi@gmail.com>
2020-04-15 19:50:19 +0530

    db config updated.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
 export class Config{
-  public static  DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://root:password@127.0.0.1:27017/admin';
+  public static  DB_CONNECTION_STRING = process.env.DATABASE_NAME ? `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASWORD}@${process.env.DB_DOMAIN}` : 'mongodb://root:password@127.0.0.1:27017/admin';
 }
