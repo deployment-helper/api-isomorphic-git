bd42e40f03650d6e5930bd678f7549ceec597793
Vinay Mavi <vinaymavi@gmail.com>
2020-04-15 13:12:03 +0530

    Logger added.

--- server.ts
+++ server.ts
@@ -3,9 +3,11 @@ import mongoose from 'mongoose';
 import { apiRouter } from './src/api'
 import { customErrorHandler } from './src/middleware'
 import {Config} from './src/config';
+import logger from './src/logger';
 /**
  * Mongodb connection 
  */
+logger.info(`Connection String ${Config.DB_CONNECTION_STRING}`);
 mongoose.connect(Config.DB_CONNECTION_STRING,{useNewUrlParser: true});
 
 const app = express();
