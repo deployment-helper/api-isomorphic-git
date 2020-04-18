86dc099cb152624e0e2d7eed230682cc665354cc
Vinay Mavi <vinaymavi@gmail.com>
2020-04-18 22:25:29 +0530

    app.sh removed from webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-  entry: './server.ts',
-  mode: 'development',
+  entry: "./server.ts",
+  mode: "development",
   target: "node",
   module: {
     rules: [
       {
         test: /\.ts?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts','.js',".json" ],
+    extensions: [".ts", ".js", ".json"],
   },
   output: {
-    filename: 'server.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "server.js",
+    path: path.resolve(__dirname, "dist"),
   },
-   plugins: [
+  plugins: [
     new CopyPlugin([
-      { from: './app.yaml', to: './app.yaml' },
-      { from: './app.js', to: './app.js' },
-      { from: './app.sh', to: './app.sh' },
-      { from: './package.json', to: './package.json' },
+      { from: "./app.yaml", to: "./app.yaml" },
+      { from: "./app.js", to: "./app.js" },
+      { from: "./package.json", to: "./package.json" },
     ]),
   ],
   externals: {
-    '@google-cloud/storage': 'commonjs @google-cloud/storage'
-},
+    "@google-cloud/storage": "commonjs @google-cloud/storage",
+  },
 };
