e29c8af5552edb0174fd74ae5a195c0adf53df44
Vinay Mavi <vinaymavi@gmail.com>
2020-08-10 14:07:38 +0530

    Controller updated

--- src/controllers/v1/auth/index.ts
+++ src/controllers/v1/auth/index.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from "express";
-import bcrypt from "bcrypt";
 import { BaseController } from "../../base.controller";
 import { regLoginSchema } from "../../../validation";
 
